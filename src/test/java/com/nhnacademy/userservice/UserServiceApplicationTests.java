12ef2ef572fdc4fe98dd37d0bb780ebcbe8b0485
Kimsongbeen <ksb10266@naver.com>
2025-04-13 17:12:18 +0900

    init

---
+++ src/test/java/com/nhnacademy/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
