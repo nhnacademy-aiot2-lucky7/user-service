205e1b2ed90b3bf24c61bd3889295c2730d6e416
Kimsongbeen <ksb10266@gmail.com>
2025-04-13 22:48:57 +0900

    수정_1.1v_완료

--- src/main/java/com/nhnacademy/controller/UserController.java
+++ src/main/java/com/nhnacademy/controller/UserController.java
@@ -4,7 +4,6 @@ import com.nhnacademy.user.dto.UserLoginRequest;
 import com.nhnacademy.user.dto.UserRegisterRequest;
 import com.nhnacademy.user.dto.UserResponse;
 import com.nhnacademy.user.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -37,29 +36,13 @@ public class UserController {
      * @param userRegisterRequest 회원가입 요청 정보 (이름, 이메일, 비밀번호 등)
      * @return 생성된 사용자 정보를 포함한 응답 (HTTP 201 Created)
      */
-    @PostMapping("/register")
-    public ResponseEntity<UserResponse> createAction(@Validated @RequestBody UserRegisterRequest userRegisterRequest){
+    @PostMapping("/signUp")
+    public ResponseEntity<Void> createAction(@Validated @RequestBody UserRegisterRequest userRegisterRequest){
         UserResponse userResponse = userService.createUser(userRegisterRequest);
         log.info("create response:{}", userResponse);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(userResponse);
-    }
-
-    /**
-     * 사용자 번호를 기반으로 사용자 정보를 조회합니다.
-     *
-     * @param encryptedUserEmail 암호화된 사용자 정보
-     * @return 사용자 정보 응답 (HTTP 200 OK)
-     */
-    @GetMapping(value = "/me")
-    public ResponseEntity<UserResponse> getAction(@RequestHeader("X-USER-ID") String encryptedUserEmail){
-
-        // 복호화하는 과정 들어가야함
-
-        UserResponse userResponse = userService.getUser(encryptedUserEmail);
-        log.info("getAction response:{}", userResponse);
-        return ResponseEntity.ok(userResponse);
+                .build();
     }
 
     /**
@@ -68,10 +51,12 @@ public class UserController {
      * @param userLoginRequest 로그인 요청 정보 (이메일, 비밀번호 등)
      * @return 로그인된 사용자 정보 응답 (HTTP 200 OK)
      */
-    @PostMapping("/login")
-    public ResponseEntity<UserResponse> loginAction(@Validated @RequestBody UserLoginRequest userLoginRequest){
+    @PostMapping("/signIn")
+    public ResponseEntity<Void> loginAction(@Validated @RequestBody UserLoginRequest userLoginRequest){
         UserResponse userResponse = userService.loginUser(userLoginRequest);
         log.info("login response:{}", userResponse);
-        return ResponseEntity.ok(userResponse);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .build();
     }
 }
